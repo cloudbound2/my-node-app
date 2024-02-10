af0d0ea6ad818dec2f947cb8ea850fb44682be63
cloudbound2 <robert.zacharski@data-minds.com>
2024-02-09 22:36:08 -0500

    initial commit of my-node-app

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, Node App on port 3200.");
+});
+
+const port = 3200;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
